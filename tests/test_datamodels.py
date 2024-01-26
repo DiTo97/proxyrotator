58a6f3da741c5f4112272271b36d1166ca4026d8
Federico Minutoli <f.minutoli@reply.it>
2024-01-26 14:49:00 +0100

    update datamodels and logging testing to 100% coverage

--- tests/test_datamodels.py
+++ tests/test_datamodels.py
@@ -15,14 +15,10 @@ def test_anonymity_from_string():
     assert Anonymity.from_string("anonymous") == Anonymity.medium
     assert Anonymity.from_string("transparent") == Anonymity.weak
     assert Anonymity.from_string("unknown") == Anonymity.unknown
-    assert Anonymity.from_string("invalid") == Anonymity.unknown
+    assert Anonymity.from_string("a1b2c3d4e5") == Anonymity.unknown
 
 
-def test_proxy_str(address: Proxy):
-    assert str(address) == "192.168.1.1:8080"
-
-
-def test_proxy_dict(address: Proxy):
+def test_proxy_asdict(address: Proxy):
     assert asdict(address) == {
         "address": "192.168.1.1",
         "port": 8080,
@@ -30,3 +26,17 @@ def test_proxy_dict(address: Proxy):
         "anonymity": "elite proxy",
         "secure": True,
     }
+
+
+def test_proxy_hash_matching(address: Proxy):
+    forward = Proxy("192.168.1.1", 8080, "EU", Anonymity.medium, False)
+    assert hash(address) == hash(forward)
+
+
+def test_proxy_hash_nonmatching(address: Proxy):
+    forward = Proxy("192.168.1.1", 8081, "US", Anonymity.medium, False)
+    assert hash(address) != hash(forward)
+
+
+def test_proxy_str(address: Proxy):
+    assert str(address) == "192.168.1.1:8080"
--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,11 +1,13 @@
 import logging
 
-import pytest
-
 from saferequests.logging import (
+    _get_library_name,
+    _get_library_root_logger,
+    _set_library_root_logger,
     get_logger,
     get_verbosity,
     set_propagation,
+    set_verbosity,
     set_verbosity_debug,
     set_verbosity_error,
     set_verbosity_info,
@@ -13,44 +15,62 @@ from saferequests.logging import (
 )
 
 
-@pytest.fixture
-def logger() -> logging.Logger:
-    return get_logger()
+def test_get_logger_name():
+    assert get_logger("testing").name == "testing"
+    assert get_logger().name == _get_library_name()
 
 
-def test_set_verbosity_info(logger: logging.Logger):
+def test_set_verbosity():
+    set_verbosity(logging.CRITICAL)
+
+    assert get_logger().getEffectiveLevel() == logging.CRITICAL
+    assert get_verbosity() == logging.CRITICAL
+
+
+def test_get_library_root_logger_name():
+    assert _get_library_root_logger().name == _get_library_name()
+
+
+def test_set_library_root_logger():
+    _set_library_root_logger()
+
+    assert get_logger().level == logging.WARNING
+    assert isinstance(get_logger().handlers[0], logging.StreamHandler)
+
+
+def test_set_verbosity_info():
     set_verbosity_info()
 
-    assert logger.getEffectiveLevel() == logging.INFO
+    assert get_logger().getEffectiveLevel() == logging.INFO
     assert get_verbosity() == logging.INFO
 
 
-def test_set_verbosity_warning(logger: logging.Logger):
+def test_set_verbosity_warning():
     set_verbosity_warning()
 
-    assert logger.getEffectiveLevel() == logging.WARNING
+    assert get_logger().getEffectiveLevel() == logging.WARNING
     assert get_verbosity() == logging.WARNING
 
 
-def test_set_verbosity_debug(logger: logging.Logger):
+def test_set_verbosity_debug():
     set_verbosity_debug()
 
-    assert logger.getEffectiveLevel() == logging.DEBUG
+    assert get_logger().getEffectiveLevel() == logging.DEBUG
     assert get_verbosity() == logging.DEBUG
 
 
-def test_set_verbosity_error(logger: logging.Logger):
+def test_set_verbosity_error():
     set_verbosity_error()
 
-    assert logger.getEffectiveLevel() == logging.ERROR
+    assert get_logger().getEffectiveLevel() == logging.ERROR
     assert get_verbosity() == logging.ERROR
 
 
-def test_set_propagation_enable(logger: logging.Logger):
+def test_set_propagation_enable():
     set_propagation(True)
-    assert logger.propagate is True
+    assert get_logger().propagate is True
 
 
-def test_set_propagation_disable(logger: logging.Logger):
+def test_set_propagation_disable():
     set_propagation(False)
-    assert logger.propagate is False
+    assert get_logger().propagate is False

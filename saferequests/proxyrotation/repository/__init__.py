ea7add4e989c7970f362d278946f554577208e13
Federico Minutoli <40361744+DiTo97@users.noreply.github.com>
2024-02-14 00:03:57 +0100

    remove hidden repository submodules

--- saferequests/proxyrotation/repository/__init__.py
+++ saferequests/proxyrotation/repository/__init__.py
@@ -40,7 +40,7 @@ class abc_Repository(ABC):
 
 def from_name(repository: str, *args: Any, **kwargs: Any) -> abc_Repository:
     """A factory method for repository import"""
-    module = f"saferequests.proxyrotation.repository._{repository}"
+    module = f"saferequests.proxyrotation.repository.{repository}"
     module = importlib.import_module(module)
 
     instance = module.Repository(*args, **kwargs)
